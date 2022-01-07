a09f319bdebc25c33a3b19d9f6634ef29da85c25
Nikos Roussos <nikos@roussos.cc>
2022-01-07 12:54:21 +0200

    Add check for current WP version

--- bin/scan.py
+++ bin/scan.py
@@ -4,7 +4,8 @@ import os
 import requests
 
 COMPOSER_FILE = 'https://raw.githubusercontent.com/greenpeace/planet4-base/main/composer.json'
-WPSCAN_API = 'https://wpscan.com/api/v3/plugins/'
+WPSCAN_PLAUGINS_API = 'https://wpscan.com/api/v3/plugins/'
+WPSCAN_WP_API = 'https://wpscan.com/api/v3/wordpresses/'
 WPSCAN_TOKEN = os.getenv('WPSCAN_TOKEN')
 WPSCAN_URL = 'https://wpscan.com/vulnerability/'
 HEADERS = {
@@ -13,30 +14,62 @@ HEADERS = {
 
 
 def plugin_check(slug, version):
-    r = requests.get('{0}{1}'.format(WPSCAN_API, slug), headers=HEADERS)
+    r = requests.get('{0}{1}'.format(WPSCAN_PLAUGINS_API, slug), headers=HEADERS)
+    plugins = r.json()
+    output = ''
+
     try:
-        vulnerabilities = r.json()[slug]['vulnerabilities']
+        vulnerabilities = plugins[slug]['vulnerabilities']
     except KeyError:
-        return False
+        return output
 
     for v in vulnerabilities:
         if (v['fixed_in'] > version):
-            return('Vulnerability affecting {0}: {1} - {2}{3}'.format(
+            output += 'Vulnerability affecting {0}: {1} - {2}{3}'.format(
                 slug,
                 v['title'],
                 WPSCAN_URL,
                 v['id'])
-            )
 
-    return False
+    return output
+
+
+def wp_check(version):
+    r = requests.get('{0}{1}'.format(WPSCAN_WP_API, version.replace('.', '')), headers=HEADERS)
+    wps = r.json()
+    output = ''
+
+    if len(wps) == 0:
+        return output
+
+    wp_major = version.replace('.', '')[:2]
+    for key, value in wps.items():
+        item = key.replace('.', '')[:2]
+        if item == wp_major:
+            vulnerabilities = value['vulnerabilities']
+            for v in vulnerabilities:
+                if (v['fixed_in'] > version):
+                    output += 'Vulnerability affecting {0}: {1} - {2}{3}'.format(
+                        slug,
+                        v['title'],
+                        WPSCAN_URL,
+                        v['id'])
+
+    return output
 
 
 if __name__ == '__main__':
     r = requests.get(COMPOSER_FILE)
     composer = json.loads(r.content)
-    requirements = composer['require']
 
+    requirements = composer['require']
+    wp_version = composer['extra']['wp-version']
     output = ''
+
+    # Check current WP version
+    output = wp_check(wp_version)
+
+    # Check all installed WP plugins
     for package in requirements:
         if 'wpackagist' in package:
             slug = package.split('/')[1]
